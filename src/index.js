871214ea5e94de238913f1d890dcdfb7c59c3a16
habasm <habasm365@gmail.com>
2023-01-05 12:04:51 +0300

    first-impression of the popup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import MealDbApi from './API_module/mealDB_API.js';
 import './style/style.css';
 
 const mealDB = new MealDbApi();
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.btn-close');
+
+// close modal function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// open modal function
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 // mealDB.fetchItems();
 // Porgarmm Starts form here:
 // =============================
@@ -47,6 +73,7 @@ const displayItems = () => {
       // Add Event for(comment) btn
       commentBtn.addEventListener('click', (e) => {
         console.log(e.target.id);
+        openModal();
       });
       // Like Count
       const divLikeTotal = document.createElement('div');
