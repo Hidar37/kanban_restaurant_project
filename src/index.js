94f70695c936d768b9eff93e1b30ee06d9d4c174
habasm <habasm365@gmail.com>
2023-01-06 11:43:54 +0300

    Popum number of comment, food image, and post messages are added

--- src/index.js
+++ src/index.js
@@ -5,11 +5,85 @@ const mealDB = new MealDbApi();
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.btn-close');
+const urlDataId = 'Lg1NwTSFJSG37nTmEN8x';
+const urlDataPostComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${urlDataId}/comments/`;
+const urlDataGetComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${urlDataId}/comments?item_id=`;
 
 // close modal function
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+
+const postDataComments = async (id, user, usercomment) => {
+  const comment = await fetch(urlDataPostComments, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+      username: user,
+      comment: usercomment,
+    }),
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  });
+  const data = await comment.json();
+  return data;
+};
+
+const countComments = (commentsObj) => {
+  const counter2 = document.querySelector('.cmnc');
+  let commentsCounter = 0;
+  commentsObj.forEach((element) => {
+    commentsCounter += 1;
+    console.log(element);
+  });
+  counter2.innerHTML = `Number of comments: ${commentsCounter}`;
+};
+
+const injetcCaptureComments = (commentsObj, id) => {
+  commentsObj.forEach((element) => {
+    const date = element.creation_date;
+    const user = element.username;
+    const usercomment = element.comment;
+
+    const comments = document.querySelector('.comments');
+    comments.innerHTML += `${date}----${user}----${usercomment}<br>`;
+  });
+  const submit = document.querySelector('.btn-pst');
+  submit.addEventListener('click', () => {
+    const username = document.getElementById('name_');
+    const usernameValue = username.value;
+    const usercomment = document.getElementById('comment_');
+    const userCommentValue = usercomment.value;
+    if (usernameValue !== null && userCommentValue !== null) {
+      postDataComments(id, usernameValue, userCommentValue);
+
+      const comments = document.querySelector('.comments');
+      const d = new Date();
+      const year = d.getFullYear();
+      const month = d.getMonth() + 1;
+      const day = d.getDay() + 3;
+
+      comments.innerHTML += `${year}-${month}-${day}----${usernameValue}----${userCommentValue}<br>`;
+      username.value = '';
+      usercomment.value = '';
+    } else {
+      const error = document.querySelector('.errormessage');
+      const errormessage = document.createElement('h4');
+      errormessage.innerHTML = 'Please, type your name and your comment. Thank you.';
+      error.appendChild(errormessage);
+    }
+  });
+};
+
+const getDataComments = async (id) => {
+  const results3 = await fetch(`${urlDataGetComments}${id}`);
+  const commentsObj = await results3.json();
+  injetcCaptureComments(commentsObj, id);
+  countComments(commentsObj);
+  console.log(commentsObj);
+  return commentsObj;
 };
 
 // close the modal when the close button and overlay is clicked
@@ -72,8 +146,19 @@ const displayItems = () => {
       commentBtn.classList.add('fa-comment', 'btn');
       // Add Event for(comment) btn
       commentBtn.addEventListener('click', (e) => {
-        console.log(e.target.id);
+        let ids = e.target.id
         openModal();
+        const cmnc = document.querySelector('.cmnc');
+        cmnc.innerHTML = '';
+        const comments = document.querySelector('.comments');
+        comments.innerHTML = '';
+
+        const img = document.querySelector('.imgs');
+        img.setAttribute('src', `${arrayElement.strMealThumb}`);
+
+        getDataComments(e.target.id);
+        document.getElementById('ftt').innerHTML = arrayElement.strMeal;
+        document.getElementById('hdvals').value = ids;
       });
       // Like Count
       const divLikeTotal = document.createElement('div');
