8a7a9adabd2d85b71b35808f27f4c0bff228e91f
habasm <habasm365@gmail.com>
2023-01-07 12:31:26 +0300

    testing case of the coments are added

---
+++ src/API_module/__tests__/commentCounter.test.js
@@ -0,0 +1,15 @@
+import mockcountComments from './commentCounter';
+
+const mockArrayComments = [
+  { comment: 'Great, food', creation_date: '2023-01-07', username: 'Maria' },
+  { comment: 'Yessssss, Nice taste', creation_date: '2023-01-07', username: 'Dario' },
+  { creation_date: '2023-01-07', comment: 'Great', username: 'Jose' },
+  { creation_date: '2023-01-07', comment: 'Great', username: 'Mauricio' },
+  { username: 'Tomas', comment: 'Great, as usual', creation_date: '2023-01-07' },
+];
+
+describe('test function countComments', () => {
+  test('count number of elemens in mockArrayComments', () => {
+    expect(mockcountComments(mockArrayComments)).toBe(5);
+  });
+});
